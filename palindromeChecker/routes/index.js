413847bc196905e04b92627ef5b953d3cb32ce3d
Tuiatua <tuiatuasiilata@gmail.com>
2024-11-19 08:48:07 -0800

    initial commit

---
+++ palindromeChecker/routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    phrase: "tacocat"
+  })
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', {
+    phrase: req.body.userText,
+    message: getResultPalindrome(req.body.userText),
+    entered: sendPalindrome(req.body.userText)
+  })
+})
+
+module.exports = router;
+
+function checkPalindrome(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("");
+  if(phrase.toLowerCase().replace(" ","") === temp.toLowerCase().replace(" ","")){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function getResultPalindrome(phrase){
+  if(checkPalindrome(phrase)){
+    return `${phrase} is a palindrome.`
+  }
+  return `${phrase} is not a palindrome.`
+}
+
+function sendPalindrome(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("")
+  temp = temp.replace(" ","")
+  return temp;
+}
