0d79b3c3780602a220ef61104db571f5edd1ae9d
Tuiatua <tuiatuasiilata@gmail.com>
2025-03-14 08:40:57 -0700

    5.2 wip

---
+++ productReviewSite/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const productController = require('../controllers/productController');
+
+/* GET home page. */
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products', productController.viewProducts);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
+
+module.exports = router;
